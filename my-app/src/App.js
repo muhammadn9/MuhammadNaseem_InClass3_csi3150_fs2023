7eda90bcc503c5ded1f1c9a15115beb7943b88fc
muhammadn9 <20mnaseem@gmail.com>
2023-11-16 13:08:51 -0500

    first commit

---
+++ my-app/src/App.js
@@ -0,0 +1,13 @@
+// App.js
+import React from "react";
+import CounterComponent from "./components/CounterComponent";
+
+const App = () => {
+  return (
+    <div>
+      <CounterComponent />
+    </div>
+  );
+};
+
+export default App;
---
+++ my-app/src/components/CounterComponent.js
@@ -0,0 +1,27 @@
+// CounterComponent.js
+import React, { useState } from "react";
+import "./CounterComponent.css";
+
+const CounterComponent = () => {
+  const [counter, setCounter] = useState(0);
+
+  const increaseCounter = () => {
+    setCounter(counter + 1);
+  };
+
+  const decreaseCounter = () => {
+    setCounter(counter - 1);
+  };
+
+  return (
+    <div className="counter-container">
+      <div className="counter-value">{counter}</div>
+      <div className="counter-buttons">
+        <button onClick={decreaseCounter}>-</button>
+        <button onClick={increaseCounter}>+</button>
+      </div>
+    </div>
+  );
+};
+
+export default CounterComponent;
